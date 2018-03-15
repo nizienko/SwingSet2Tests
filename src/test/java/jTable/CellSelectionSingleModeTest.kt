0a889ef872b799988f60236e2fc8d68b7e2bcabc
nizienko <nizienko@outlook.com>
2018-03-15 23:21:09 +0300

    Single mode selection tests
    Added cell selection tests
    
    kotlin version 1.2.21 -> 1.2.30
    kotlin-test -> kotlin-test-junit

--- src/test/java/jTable/CellSelectionSingleModeTest.kt
+++ src/test/java/jTable/CellSelectionSingleModeTest.kt
@@ -286,4 +286,196 @@ class CellSelectionSingleModeTest : JTableTestSuite() {
             columnIsNotSelected(movedCell.column, exceptCellAtRow = movedCell.row)
         }
     }
+
+    @Test
+    fun onlyOneCellSelectedWhenBothComboBoxesChecked(): Unit = with(SwingSet2.jTablePanel) {
+        rowSelectionCheckBox.check()
+        columnSelectionCheckBox.check()
+
+        table.clickCells {
+            cell(7, 1)
+            pressingControl {
+                cell(9, 3)
+            }
+        }
+        table.checkCellsSelection {
+            cellIsNotSelected(7, 1)
+            rowIsNotSelected(9, exceptCellAtColumn = 3)
+            columnIsNotSelected(3, exceptCellAtRow = 9)
+        }
+    }
+
+    @Test
+    fun onlyOneCellFromRangeSelectedWhenBothComboBoxesChecked(): Unit = with(SwingSet2.jTablePanel) {
+        rowSelectionCheckBox.check()
+        columnSelectionCheckBox.check()
+
+        table.clickCells {
+            cell(7, 1)
+            pressingShift {
+                cell(9, 3)
+            }
+        }
+        table.checkCellsSelection {
+            cellSelected(9, 3)
+            columnIsNotSelected(1)
+            columnIsNotSelected(2)
+            columnIsNotSelected(3, exceptCellAtRow = 9)
+            rowIsNotSelected(7)
+            rowIsNotSelected(8)
+            rowIsNotSelected(9, exceptCellAtColumn = 3)
+        }
+    }
+
+    @Test
+    fun cellUnSelectedWithSecondClick(): Unit = with(SwingSet2.jTablePanel) {
+        rowSelectionCheckBox.check()
+        columnSelectionCheckBox.check()
+
+        table.clickCells {
+            cell(9, 5)
+            pressingControl {
+                cell(9, 5)
+            }
+        }
+        table.checkCellsSelection {
+            cellIsNotSelected(9, 5)
+        }
+    }
+
+
+    @Test
+    fun reorderingKeepsCellSelection(): Unit = with(SwingSet2.jTablePanel) {
+        rowSelectionCheckBox.check()
+        columnSelectionCheckBox.check()
+        reorderingAllowedCheckBox.check()
+
+        table.clickCells {
+            cell(7, 1)
+        }
+        table.dragColumnToAnother(fromIndex = 1, toIndex = 4)
+        table.checkCellsSelection {
+            cellIsNotSelected(7, 1)
+            cellSelected(7, 4)
+        }
+    }
+
+    @Test
+    fun reorderingFromAnotherColumnKeepsCellSelection(): Unit = with(SwingSet2.jTablePanel) {
+        rowSelectionCheckBox.check()
+        columnSelectionCheckBox.check()
+        reorderingAllowedCheckBox.check()
+
+        table.clickCells {
+            cell(7, 1)
+        }
+        table.dragColumnToAnother(fromIndex = 4, toIndex = 1)
+        table.checkCellsSelection {
+            cellIsNotSelected(7, 1)
+            cellSelected(7, 2)
+        }
+    }
+
+    @Test
+    fun sortingKeepsCellSelection(): Unit = with(SwingSet2.jTablePanel) {
+        rowSelectionCheckBox.check()
+        columnSelectionCheckBox.check()
+        table.tableHeader().clickColumn(0)
+        val firstName = table.cell(TableCell.row(7).column(0)).value()
+        table.clickCells {
+            cell(7, 1)
+        }
+        table.tableHeader().clickColumn(1)
+
+        val movedCell = table.cell(firstName)
+
+        table.checkCellsSelection {
+            cellSelected(movedCell.row, 1)
+            cellIsNotSelected(7, 1)
+        }
+    }
+
+    @Test
+    fun convertCellToColumn(): Unit = with(SwingSet2.jTablePanel) {
+        rowSelectionCheckBox.check()
+        columnSelectionCheckBox.check()
+        table.clickCells {
+            cell(7, 1)
+        }
+        rowSelectionCheckBox.uncheck()
+        table.checkCellsSelection {
+            rowIsNotSelected(7, exceptCellAtColumn = 1)
+            columnSelected(1)
+        }
+    }
+
+    @Test
+    fun convertColumnToCell(): Unit = with(SwingSet2.jTablePanel) {
+        rowSelectionCheckBox.uncheck()
+        columnSelectionCheckBox.check()
+        table.clickCells {
+            cell(7, 1)
+        }
+        rowSelectionCheckBox.check()
+        table.checkCellsSelection {
+            rowIsNotSelected(7, exceptCellAtColumn = 1)
+            columnIsNotSelected(1, exceptCellAtRow = 7)
+        }
+    }
+
+    @Test
+    fun convertCellToRow(): Unit = with(SwingSet2.jTablePanel) {
+        rowSelectionCheckBox.check()
+        columnSelectionCheckBox.check()
+        table.clickCells {
+            cell(7, 1)
+        }
+        columnSelectionCheckBox.uncheck()
+        table.checkCellsSelection {
+            columnIsNotSelected(1, exceptCellAtRow = 7)
+            rowSelected(7)
+        }
+    }
+
+    @Test
+    fun convertRowToCell(): Unit = with(SwingSet2.jTablePanel) {
+        rowSelectionCheckBox.check()
+        columnSelectionCheckBox.uncheck()
+        table.clickCells {
+            cell(7, 1)
+        }
+        columnSelectionCheckBox.check()
+        table.checkCellsSelection {
+            rowIsNotSelected(7, exceptCellAtColumn = 1)
+            columnIsNotSelected(1, exceptCellAtRow = 7)
+        }
+    }
+
+    @Test
+    fun unSelectWithCheckBoxes(): Unit = with(SwingSet2.jTablePanel) {
+        rowSelectionCheckBox.check()
+        columnSelectionCheckBox.check()
+        table.clickCells {
+            cell(7, 1)
+        }
+        rowSelectionCheckBox.uncheck()
+        columnSelectionCheckBox.uncheck()
+        table.checkCellsSelection {
+            cellIsNotSelected(7, 1)
+        }
+    }
+
+    @Test
+    fun switchOnCheckBoxes(): Unit = with(SwingSet2.jTablePanel) {
+        rowSelectionCheckBox.uncheck()
+        columnSelectionCheckBox.uncheck()
+        table.clickCells {
+            cell(7, 1)
+        }
+        rowSelectionCheckBox.check()
+        columnSelectionCheckBox.check()
+        table.checkCellsSelection {
+            cellSelected(7, 1)
+        }
+    }
 }
