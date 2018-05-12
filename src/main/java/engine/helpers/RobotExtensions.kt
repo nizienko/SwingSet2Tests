dcc0a94312a0cb7187df3318fe7b58dc60689dd6
nizienko <nizienko@outlook.com>
2018-05-12 16:42:31 +0300

    Review SWNGS-CR-2 commentId=ef102512-7ee8-4811-8686-a53fdce792b8
    Deleted redundant BigDecimal in moveMouseGradually function

--- src/main/java/engine/helpers/RobotExtensions.kt
+++ src/main/java/engine/helpers/RobotExtensions.kt
@@ -3,28 +3,33 @@ package engine.helpers
 import org.fest.swing.core.MouseButton
 import org.fest.swing.core.Robot
 import java.awt.Point
-import java.math.BigDecimal
-import java.math.RoundingMode
 import kotlin.math.absoluteValue
 
+/**
+ * Move mouse from point to point.
+ * It is possible to manipulate speed by defining skippedPixels parameter.
+ *
+ * @param from starting point
+ * @param to finishing at that point
+ * @param skippedPixels 1 by default - pointer will move to every pixel of path from start point to finish.
+ *  With values > 1 mouse will jump what will increase speed
+ */
 fun Robot.moveMouseGradually(from: Point, to: Point, skippedPixels: Int = 1) {
     val xLength = (to.x - from.x).absoluteValue
     val yLength = (to.y - from.y).absoluteValue
 
-    val k = try {
-        BigDecimal(from.y - to.y).divide(BigDecimal(from.x - to.x), 2, RoundingMode.DOWN)
-    } catch (e: ArithmeticException) { BigDecimal.ZERO }
+    val k = ((from.y - to.y).toDouble()) / (from.x - to.x)
 
-    val b = BigDecimal(from.y).subtract(k.multiply(BigDecimal(from.x)))
+    val b = from.y - k * from.x
 
     if (xLength > yLength) {
-        val yFun: (x: Int) -> Int = { k.multiply(BigDecimal(it)).add(b).toInt() }
+        val yFun: (x: Int) -> Int = { (k * it + b).toInt() }
         for (x in from.x goingTo to.x step skippedPixels) {
             moveMouse(x, yFun(x))
         }
     } else {
-        val xFun: (y: Int) -> Int = if (k != BigDecimal.ZERO) {
-            { BigDecimal(it).subtract(b).divide(k, 0, RoundingMode.DOWN).toInt() }
+        val xFun: (y: Int) -> Int = if (k != 0.0) {
+            { it - (b / k).toInt() }
         } else {
             { from.x }
         }
